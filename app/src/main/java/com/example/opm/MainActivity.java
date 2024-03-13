cce7d1db9705a7fe25849ec46b8fa31b9064d514
ArtemAITM <antropovam2008@gmail.com>
2024-03-13 20:30:58 +0700

    new version

--- app/src/main/java/com/example/opm/MainActivity.java
+++ app/src/main/java/com/example/opm/MainActivity.java
@@ -1,19 +1,17 @@
 package com.example.opm;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.accounts.AbstractAccountAuthenticator;
-import android.content.Intent;
-import android.graphics.Color;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 
 import com.example.opm.databinding.ActivityMainBinding;
-import com.fathzer.soft.javaluator.DoubleEvaluator;
-import com.fathzer.soft.javaluator.StaticVariableSet;
-import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
@@ -24,20 +22,64 @@ public class MainActivity extends AppCompatActivity {
     private LineDataSet datasetEmpty = new LineDataSet(entriesempty, "График первый");
     private ActivityMainBinding binding;
     private ArrayList<ILineDataSet> dataSetsEmpty = new ArrayList();
-    private BuildMathFuntction funtction = new BuildMathFuntction();
+    private BuildMathFuntction drawing = new BuildMathFuntction();
+    private ArrayList<String> FunctionsList;
+    private ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        funtction.nullChart(binding);
+        start();
+
         binding.draw.setOnClickListener(v -> {
-            try {
-                funtction.draw(binding);
-            } catch (InstantiationException e) {
-                throw new RuntimeException(e);
+            for (int i = 0; i < FunctionsList.size(); i++) {
+                drawing.draw(FunctionsList.get(i), i);
+                drawing.draw_all(binding);
             }
         });
+
+        binding.plus.setOnClickListener(v -> {
+            String text = binding.enterfunction.getText().toString();
+            FunctionsList.add(text);
+            adapter.notifyDataSetChanged();
+        });
+    }
+
+    protected void start(){
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        //funtction.nullChart(binding);
+        FunctionsList = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, R.layout.function_item, R.id.FunctionString, FunctionsList);
+        binding.listFunctions.setAdapter(adapter);
+        binding.listFunctions.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                String pos = FunctionsList.get(position);
+                delFunction(pos);
+                return false;
+            }
+        });
+    }
+
+    private void delFunction(String pos) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Удалить функцию?");
+        builder.setPositiveButton("Да", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                FunctionsList.remove(pos);
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        builder.setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
     }
 
 }
