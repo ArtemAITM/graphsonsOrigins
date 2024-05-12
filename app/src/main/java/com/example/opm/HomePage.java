09fdf65123281f5b9589e15d66bd471f6116673f
ArtemAITM <antropovam2008@gmail.com>
2024-05-12 17:09:05 +0700

    Creating a finish design for app

--- app/src/main/java/com/example/opm/HomePage.java
+++ app/src/main/java/com/example/opm/HomePage.java
@@ -48,7 +48,7 @@ public class HomePage extends AppCompatActivity {
         System.out.println(calendar.get(Calendar.MONTH));
         String visitData = calendar.get(Calendar.YEAR) + ":"
                 + (calendar.get(Calendar.MONTH) + 1) + ":"
-                + calendar.get(Calendar.DAY_OF_MONTH) + ":"
+                + calendar.get( Calendar.DAY_OF_MONTH) + ":"
                 + calendar.get(Calendar.HOUR_OF_DAY);
 
         String visitId = visitsRef.push().getKey();
